46b0df41e2ec97286ffcc474ee8da502178a08d5
hrsh9 <rex@HARSHs-MacBook-Pro.local>
2022-02-17 16:35:16 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,52 @@
+var p1 = "Player 1";
+var p2 = "Player 2";
+  
+//change player's name
+function editPlayerNames() {
+    p1 = prompt("Enter 1st Player's Name:");
+    p2 = prompt("Enter 2nd player's Name:");
+  
+    document.querySelector("p.1stPlayer").innerHTML = p1;
+    document.querySelector("p.2ndPlayer").innerHTML = p2;
+}
+  
+//to roll dice 
+function roll() {
+    setTimeout(function () {
+        var player1 = Math.floor(Math.random() * 6) + 1;
+        var player2 = Math.floor(Math.random() * 6) + 1;
+  
+        document.querySelector(".img1").setAttribute("src",
+            "dice/dice" + player1 + ".png");
+  
+        document.querySelector(".img2").setAttribute("src",
+            "dice/dice" + player2 + ".png");
+  
+        if (player1 === player2) {
+            document.querySelector("h3").innerHTML = "Draw!";
+            const para = document.createElement("div");
+            const node = document.createTextNode("Draw");
+            para.appendChild(node);
+            const element = document.getElementById("history");
+            element.appendChild(para);
+        }
+  
+        else if (player1 < player2) {
+            document.querySelector("h3").innerHTML = (p2 + " WINS!");
+            const para = document.createElement("div");
+            const node = document.createTextNode(p2 + " Wins!");
+            para.appendChild(node);
+            const element = document.getElementById("history");
+            element.appendChild(para);
+        }
+  
+        else {
+            document.querySelector("h3").innerHTML = (p1 + " WINS!");
+            const para = document.createElement("div");
+            const node = document.createTextNode(p1 + " Wins");
+            para.appendChild(node);
+            const element = document.getElementById("history");
+            element.appendChild(para);
+        }
+    }, 0);
+}
